ac6d09755b777709eb0fd35ebfd9692495d0a17e
GrahamMJohnson <gmjsoccer@gmail.com>
2025-06-16 17:23:24 -0500

    Added parentheses to keypad and redid layout

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -17,68 +17,65 @@ const Keypad = ({
   return (
     <div className="d-grid gap-2">
       <div className="row g-2">
-        {/* 1-3 number buttons */}
-        <div className="col-4">
+        {/* 1-4 number buttons */}
+        <div className="col-3">
           <Button label="1" onClick={() => onNumberClick("1")} />
         </div>
-        <div className="col-4">
+        <div className="col-3">
           <Button label="2" onClick={() => onNumberClick("2")} />
         </div>
-        <div className="col-4">
+        <div className="col-3">
           <Button label="3" onClick={() => onNumberClick("3")} />
         </div>
-      </div>
-      <div className="row g-2">
-        {/* 4-6 number buttons */}
-        <div className="col-4">
+        <div className="col-3">
           <Button label="4" onClick={() => onNumberClick("4")} />
         </div>
-        <div className="col-4">
+      </div>
+      <div className="row g-2">
+        {/* 5-7 number buttons */}
+        <div className="col-3">
           <Button label="5" onClick={() => onNumberClick("5")} />
         </div>
-        <div className="col-4">
+        <div className="col-3">
           <Button label="6" onClick={() => onNumberClick("6")} />
         </div>
-      </div>
-      <div className="row g-2">
-        {/* 7-9 number buttons */}
-        <div className="col-4">
+        <div className="col-3">
           <Button label="7" onClick={() => onNumberClick("7")} />
         </div>
-        <div className="col-4">
+      </div>
+      <div className="row g-2">
+        {/* 8-0 number buttons */}
+        <div className="col-3">
           <Button label="8" onClick={() => onNumberClick("8")} />
         </div>
-        <div className="col-4">
+        <div className="col-3">
           <Button label="9" onClick={() => onNumberClick("9")} />
         </div>
-      </div>
-      <div className="row g-2">
-        {/* 0, clear and equals buttons */}
-        <div className="col-4">
+        <div className="col-3">
           <Button label="0" onClick={() => onNumberClick("0")} />
         </div>
-        <div className="col-4">
-          <Button label="C" onClick={onClear} variant="danger" />
-        </div>
-        <div className="col-4">
-          <Button label="=" onClick={onEquals} variant="success" />
-        </div>
       </div>
       <div className="row g-2">
-        {/* parentheses buttons */}
-        <div className="col-6">
+        {/* parentheses, clear and equals buttons */}
+        <div className="col-3">
           <Button
             label="("
             onClick={() => onOperationClick("(")}
             variant="primary"
           />
         </div>
-        <div className="col-6">
+        <div className="col-3">
           <Button
             label=")"
             onClick={() => onOperationClick(")")}
             variant="primary"
           />
+        </div>
+        <div className="col-3">
+          <Button label="C" onClick={onClear} variant="danger" />
+        </div>
+        <div className="col-3">
+          <Button label="=" onClick={onEquals} variant="success" />
         </div>
       </div>
       <div className="row g-2 mt-2">
